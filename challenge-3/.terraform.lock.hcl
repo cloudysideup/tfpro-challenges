0b0190a89a60dd61a6428ce9c8aacafa17a38f98
Jason Elliott <54967024+cloudysideup@users.noreply.github.com>
2026-07-16 22:07:34 -0500

    base task and task 1

---
+++ challenge-3/.terraform.lock.hcl
@@ -0,0 +1,45 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/aws" {
+  version     = "5.80.0"
+  constraints = "5.80.0"
+  hashes = [
+    "h1:N5Wfsf4xe5DJfSeo0G/ulkIxzyfmUIoSj/hAiZ2DaKU=",
+    "zh:0b1655e39639d60f2de2860a5df8642f9556ba0ca04529c1b861fde4935cb0df",
+    "zh:13dc0155e0a11edceee29ce687fc04c5a5a85f3324c67556472713cfd52e5807",
+    "zh:180f6cb2be44be14cfe329e0649121b774319f083b6e4e8fb749f85090d73121",
+    "zh:3158d44b74c67465f7f19f22c42b643840c8d18ce833e2ec86e8d93085b06926",
+    "zh:6351b5bf7cde5dc83e926944891570636069e05ca43341f4d1feda67773469bf",
+    "zh:6fa9db1532096ba50e842d369b6688979306d2295c7ead49b8a266b0d60962cc",
+    "zh:85d2fe75def7619ff2cc29102048875039cad088fafb62ecc14c3763e7b1e9d9",
+    "zh:9028d653f1d7341c6dfe2afe961b6541581e9043a474eac2faf90e6426a24f6d",
+    "zh:9b12af85486a96aedd8d7984b0ff811a4b42e3d88dad1a3fb4c0b580d04fa425",
+    "zh:9c4e248c442bc60f07f9f089e5361f19936833370dc3c04b27916672b765f0e1",
+    "zh:a710a3979596e3f3938c3ec6bb748e604724d3a4afa96ed2c14f0a245cc41a11",
+    "zh:c27936bdf447779d0c0833bf52a9ef618985f5ea8e3e243d6266513520ca31c4",
+    "zh:c7681134a123486e72eaedc3f8d2d75e267dbbfd45fa7de5aea8f757af57f89b",
+    "zh:ea717ebad3561fd02591f9eecf30f3df5635405556fba2bdbf29fd42691bebac",
+    "zh:f4e1e8f23c58c3e8f4371f9c3379a723ab4155246e6b6daad8eb99e16666b2cb",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/local" {
+  version = "2.9.0"
+  hashes = [
+    "h1:9rBZCMNpxKwMlRbWH2QpwD3kqUCAejdOZQ/aiiDObXQ=",
+    "zh:0baa4566cf77f1ff52f4293d1c8536202dd23edc197c3196413a28343c3ac3a0",
+    "zh:16b5559c3c07088ddad11a9bb9e9c0799999363c2958e9a5be2bcbbf2cd9ca64",
+    "zh:197c79015a10d1cce904a8ea722cbc750c42aeae2da53f44a6a0751d9fd1aa90",
+    "zh:29d0b03e5343a80677ebfeb2e2c31cbe4b1f65e736e53417454a4277fec2544c",
+    "zh:4896bfa6cf1d2fd562b47ef2e87f47862ae92a04f8ad5d764380f0c6653473b8",
+    "zh:531f8529cbca49f681883e57761a05a8398afaef6d1ab0d205d26bf12f4428e8",
+    "zh:6aaf5011d83161c86d2bfb80c0923ec934e578288758da2f37acb7aec129004b",
+    "zh:7430275253d3d3c40aa6179e0ec0d63212874dbbc06c5a51b9d07ec590f9756c",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:be17dc611e95e26cdf6cad79dfccf1064f0e32032a2efeb939a9bbe7fb1cbfe9",
+    "zh:f0e3b0aa644202e1d79d2000dca91f6019425da71e9800fa23f27e51c034f195",
+    "zh:f62bae4519e4ead49182ddc8afe8cf61e2a4c3ba3973b0fbba967736a2696aa3",
+    "zh:fcafa360a5b0b96244f26f4e3a6d642b716a376557142c2442ff2fb12d11da18",
+  ]
+}
--- challenge-3/challenge-3.tf
+++ challenge-3/challenge-3.tf
@@ -1,66 +1,91 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.80.0"
     }
   }
 }
 
 provider "aws" {
- region = "us-east-1"
+  region = "us-east-1"
 }
 
-
-resource "aws_launch_template" "this" {
-    name     = "terraform-launch-template"
-    image_id = "ami-06b21ccaeff8cd686"
-    instance_type = "t2.micro"
+module "asg" {
+  source = "./modules/asg/"
 }
 
-resource "aws_autoscaling_group" "dev" {
-  availability_zones = ["us-east-1a","us-east-1b"]
-  desired_capacity   = 1
-  max_size           = 2
-  min_size           = 1
-
-  launch_template {
-    id      = aws_launch_template.this.id
-    version = "$Latest"
-  }
-
-  tag {
-    key   = "Team"
-    value = "SRE"
-    propagate_at_launch = true
-  }
+module "iam" {
+  source = "./modules/iam/"
 }
 
-resource "aws_iam_user" "lb" {
-  count = 1
-  name = "success-user"
+#resource "aws_launch_template" "this" {
+#  name          = "terraform-launch-template"
+#  image_id      = "ami-06b21ccaeff8cd686"
+#  instance_type = "t2.micro"
+#}
+#
+#resource "aws_autoscaling_group" "dev" {
+#  availability_zones = ["us-east-1a", "us-east-1b"]
+#  desired_capacity   = 1
+#  max_size           = 2
+#  min_size           = 1
+#
+#  launch_template {
+#    id      = aws_launch_template.this.id
+#    version = "$Latest"
+#  }
+#
+#  tag {
+#    key                 = "Team"
+#    value               = "SRE"
+#    propagate_at_launch = true
+#  }
+#}
+
+moved {
+  from = aws_launch_template.this
+  to   = module.asg.aws_launch_template.this
 }
 
-resource "aws_iam_user_policy" "lb_ro" {
-  name = "ec2-describe-policy"
-  user = "success-user"
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ec2:Describe*",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
+moved {
+  from = aws_autoscaling_group.dev
+  to   = module.asg.aws_autoscaling_group.dev
 }
 
+#resource "aws_iam_user" "lb" {
+#  count = 1
+#  name  = "success-user"
+#}
+#
+#resource "aws_iam_user_policy" "lb_ro" {
+#  name = "ec2-describe-policy"
+#  user = "success-user"
+#  policy = jsonencode({
+#    Version = "2012-10-17"
+#    Statement = [
+#      {
+#        Action = [
+#          "ec2:Describe*",
+#        ]
+#        Effect   = "Allow"
+#        Resource = "*"
+#      },
+#    ]
+#  })
+#}
+moved {
+  from = aws_iam_user.lb
+  to   = module.iam.aws_iam_user.lb
+}
+
+moved {
+  from = aws_iam_user_policy.lb_ro
+  to   = module.iam.aws_iam_user_policy.lb_ro
+}
 data "aws_caller_identity" "local" {}
 
 resource "local_file" "this" {
-  content = data.aws_caller_identity.local.account_id
+  content  = data.aws_caller_identity.local.account_id
   filename = "account-number.txt"
 }

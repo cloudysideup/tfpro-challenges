c251d87386042aa3b0d50d67df877caa2e864819
Jason Elliott <54967024+cloudysideup@users.noreply.github.com>
2026-07-28 06:30:50 -0500

    ministack setup

--- challenge-1/providers.tf
+++ challenge-1/providers.tf
@@ -1,8 +1,24 @@
+#provider "aws" {
+#  region = var.region
+#  default_tags {
+#    tags = {
+#      Environment = var.environment
+#    }
+#  }
+#}
+
 provider "aws" {
-  region = var.region
-  default_tags {
-    tags = {
-      Environment = var.environment
-    }
+  access_key                  = "test"
+  secret_key                  = "test"
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    s3       = "http://localhost:4566"
+    dynamodb = "http://localhost:4566"
+    lambda   = "http://localhost:4566"
+    # Add other services needed...
   }
 }

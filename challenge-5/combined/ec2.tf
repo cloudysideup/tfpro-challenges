d065ee2e3d6508405bc1dbc32c5407a1a2ada171
Jason Elliott <54967024+cloudysideup@users.noreply.github.com>
2026-08-01 07:11:11 +0100

    placeholders for data block ref

--- challenge-5/combined/ec2.tf
+++ challenge-5/combined/ec2.tf
@@ -5,4 +5,16 @@
 #
 #> [!NOTE]
 #> You need to reference to subnet_id by querying data source. No hardcoding.
-#
+
+
+resource "aws_instance" "example" {
+  for_each      = data.aws_subnet.subnet_ids.id
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  subnet_id = each.key
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
